f5a09243d0cd1f07b2c9027f343976031230e57e
Lucia <analucia0189@gmail.com>
2015-06-02 17:59:39 -0600

    funciona

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import os
 
-import jinja2
 import webapp2
 
-from google.appengine.api import taskqueue
 from google.appengine.ext import ndb
+from google.appengine.api import mail
+from datetime import datetime
 
-class persona(ndb.Model):
-    nombre = ndb.StringProperty(indexed = True)
-    edad = ndb.IntegerProperty(indexed=False)
-        
-class CounterWorker(webapp2.RequestHandler):
+class NotifConfig(ndb.Model):
+    notification_type = ndb.StringProperty()
+    notification_date = ndb.DateProperty()
+     
+class DataRetriever(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write('llego aqui')
-        personas = persona.query()
-        print(personas)
-        for p in personas:
-            print(p)
-            self.response.out.write(p.nombre)
+        #if self.request.headers.get('X-AppEngine-Cron'):
+            today = datetime.today().replace(hour=00,minute=00,second=00,microsecond=0)
+            notifications = NotifConfig.query(NotifConfig.notification_date == today)
+            print notifications
+            for n in notifications:
+                mail.send_mail(sender="Lucia analucia0189@gmail.com",
+                    to="analucia0189@gmail.com",
+                    subject="Email notif",
+                    body="Hoy es dia de notificacion!!!")
 
 
 
 APP = webapp2.WSGIApplication(
     [
-        ('/luciatest', CounterWorker)
-    ], debug=True)
+        ('/retrievedata', DataRetriever)
+    ])
